47d915d02592bbc5d03cc4ed7db8a77773ef327f
ryoga <surpace2479@gmail.com>
2020-02-14 13:30:48 +0900

    add: データのトリム機能

--- RADAR_simulator.m
+++ RADAR_simulator.m
@@ -1,8 +1,12 @@
 clc;clear;
 
+%% 読み込む範囲の設定
+start_time = 15;
+end_time = 35;
+
+
 %% 読み込むデータの選択
 select_data = 'test_data/non-LPF-High';
-
 
 %% RADRAデータの読み込み
 % CSVから行列データを生成
@@ -22,21 +26,40 @@ radar_time_data(1,:) = [];
 radar_dis_data = rada_raw_data(1,:);
 radar_dis_data(:,1) = [];
 
-% Simulinkに読ませるデータの生成
-SimIn.signals.values = radar_amp_data;
-SimIn.signals.dimensions = size(SimIn.signals.values,2);
-SimIn.time = radar_time_data;
+% end_time = radar_time_data(end);
 
-
-%% ジャイロデータの読み込み 
+%% 加速度データの読み込み 
 acc_raw_data = readmatrix(strcat(select_data, '/acc.csv'));
 
 acc_data = acc_raw_data;
 acc_data(1,:) = [];
 acc_data(:,1) = [];
 
+
+%% データのトリミング 
+for i = 1:size(radar_time_data)
+    if radar_time_data(i) >= start_time 
+        radar_time_data(1:i) = [];
+        radar_amp_data(1:i,:) = [];
+        acc_data(1:i) = [];
+        break;
+    end
+end
+
+for i = 1:size(radar_time_data)
+    if radar_time_data(i) >= end_time 
+        radar_time_data(i:end) = [];        
+        radar_amp_data(i:end,:) = [];
+        acc_data(i:end,:) = [];
+        break;
+    end
+end
 acc_time_data = radar_time_data(:,1);
-acc_time_data(1,:) = [];
+
+%% Simulinkに読ませるデータの生成
+SimIn.signals.values = radar_amp_data;
+SimIn.signals.dimensions = size(SimIn.signals.values,2);
+SimIn.time = radar_time_data;
 
 simin_acc.signals.values = acc_data;
 simin_acc.signals.dimensions = size(simin_acc.signals.values,2);
@@ -47,11 +70,8 @@ simin_acc.time = acc_time_data;
 subplot(2,1,1)
 mesh(radar_time_data, radar_dis_data, radar_amp_data.')
 view(2)
-xlim([25 35])
-ylim([0.1 0.5])
 subplot(2,1,2)
 plot(acc_time_data, acc_data)
-xlim([25 35])
 
 
 %% パラメータ
