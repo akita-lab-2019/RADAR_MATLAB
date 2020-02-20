d31ca13303ff09df49bbaeb23eb5878cad553f0f
ryoga <surpace2479@gmail.com>
2020-02-20 13:24:54 +0900

    update

--- RADAR_simulator.m
+++ RADAR_simulator.m
@@ -4,69 +4,35 @@ clc;clear;
 start_time = 15;
 end_time = 35;
 
-
 %% 読み込むデータの選択
 select_data = 'test_data/non-LPF-High';
 
 %% RADRAデータの読み込み
 % CSVから行列データを生成
-rada_raw_data = readmatrix(strcat(select_data, '/3.csv'));
-rada_raw_data(end,:) = [];
-
-% 振幅のみの行列にする
-radar_amp_data = rada_raw_data;
-radar_amp_data(1,:) = [];
-radar_amp_data(:,1) = [];
-
-% 時間のベクトルを生成
-radar_time_data = rada_raw_data(:,1);
-radar_time_data(1,:) = [];
-
-% 距離のベクトルを生成
-radar_dis_data = rada_raw_data(1,:);
-radar_dis_data(:,1) = [];
-
-% end_time = radar_time_data(end);
+radar_raw_data = readmatrix(strcat(select_data, '/3.csv'));
+radar_raw_data(end,:) = [];
+radar_amp_data = radar_raw_data(2:end,2:end);
+radar_time_data = radar_raw_data(2:end,1);
+radar_dis_data = radar_raw_data(1,2:end);
 
 %% 加速度データの読み込み 
 acc_raw_data = readmatrix(strcat(select_data, '/acc.csv'));
-
-acc_data = acc_raw_data;
-acc_data(1,:) = [];
-acc_data(:,1) = [];
+acc_raw_data(end,:) = [];
+acc_time_data = acc_raw_data(:,1);
+acc_data = acc_raw_data(:,2);
 
 %% Simulinkに読ませるデータの生成
 SimIn = createSimuIn(radar_time_data, radar_amp_data);
-simin_acc = createSimuIn(radar_time_data, acc_data);
-
-%% データのトリミング 
-% for i = 1:size(radar_time_data)
-%     if radar_time_data(i) >= start_time 
-%         radar_time_data(1:i) = [];
-%         radar_amp_data(1:i,:) = [];
-%         acc_data(1:i) = [];
-%         break;
-%     end
-% end
-% 
-% for i = 1:size(radar_time_data)
-%     if radar_time_data(i) >= end_time 
-%         radar_time_data(i:end) = [];        
-%         radar_amp_data(i:end,:) = [];
-%         acc_data(i:end,:) = [];
-%         break;
-%     end
-% end
-acc_time_data = radar_time_data(:,1);
-acc_time_data(end,:) = [];
+Simin_dis = createSimuIn(0, radar_dis_data);
+simin_acc = createSimuIn(acc_time_data, acc_data);
 
 %% グラフの描画
 subplot(2,1,1)
-mesh(radar_time_data, radar_dis_data, radar_amp_data.')
+mesh(SimIn.time, radar_dis_data, SimIn.signals.values.')
 view(2)
 subplot(2,1,2)
-plot(acc_time_data, acc_data)
-
+plot(radar_dis_data, SimIn.signals.values)
+% plot(simin_acc.time, simin_acc.signals.values)
 
 %% パラメータ
 stopTime = SimIn.time(end);
